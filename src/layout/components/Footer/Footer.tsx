c8f9be5ca439031e8ea62ebdd173fea94fe7e617
Lê Sinh Hùng <111550586+slimz-dev@users.noreply.github.com>
2025-08-18 20:17:24 +0700

    Update Footer.tsx

--- src/layout/components/Footer/Footer.tsx
+++ src/layout/components/Footer/Footer.tsx
@@ -222,7 +222,7 @@ const Footer = (): JSX.Element => {
 			<div
 				ref={bannerRef}
 				style={{ transform: 'translateY(-50%)' }}
-				className="absolute max-[500px]:left-0 max-[500px]:right-0 max-lg:flex-col max-xl:justify-center flex-wrap w-fit  left-20 top-0 right-20 flex justify-between bg-black text-white p-10 max-sm:p-5 px-20 rounded-xl"
+				className="absolute max-[500px]:left-0 max-[500px]:right-0 max-lg:flex-col max-xl:justify-center flex-wrap  left-20 top-0 right-20 flex justify-between bg-black text-white p-10 max-sm:p-5 px-20 rounded-xl"
 			>
 				<div className="font-[integral-cf] text-5xl w-[44%] max-xl:w-full max-lg:text-4xl max-md:text-3xl max-sm:text-2xl max-xl:mb-6">
 					Stay upto date about our latest offers
